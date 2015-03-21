1e6fba84ecd9ef533af36c11fd89e84ed38b9f0b
codefisher <mail@codefisher.org>
2015-03-21 22:08:11 +0100

    now asks for more then just one application, and also asks if a script should be included

--- start_button.py
+++ start_button.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import re
 
 from builder.util import apply_settings_files
 
@@ -8,6 +9,9 @@ try:
 except ImportError:
     print "Failed to load settings."
     sys.exit(1)
+    
+def function_name(match):
+    return match.group(1).upper()
 
 def main():
     args = sys.argv[1:]
@@ -19,6 +23,7 @@ def main():
          ("fx", "browser"),
          ("tb", "messenger"),
          ("sb", "calendar"),
+         ("sm", "suite_browser"),
          ("b", "button"),
     )
     
@@ -45,25 +50,30 @@ def main():
                 pass
     
     button_id = raw_input("What ID do you want to use: ").strip()
+    function = re.sub(r"[^a-zA-Z]+([a-zA-Z])", function_name, button_id)
     label = raw_input("What label: ").strip()
     tooltip = raw_input("What tooltip: ").strip()
     description = raw_input("What description: ").strip()
     icon = raw_input("What icon: ").strip()
-    
     button_id = config.get("new_button_prefix", "") + button_id
     
-    print("Please select what application you want to support (you can add more later).")
+    print("Please select what application you want to support (you can add more later).\nPress enter to finish.")
     for i, name in files:
         print("    [{0}] {1}".format(i, name))
+    apps = []
     while True:
         try:
-            selection = raw_input("Application: ")
+            selection = raw_input("Application: ").strip()
             if selection in dfiles:
-                app = dfiles[selection]
+                apps.append(dfiles[selection])
+            elif not selection:
                 break
         except ValueError:
             pass
-            
+    
+    add_script = raw_input("Do you want to add a script file? Y/N: ")
+    add_js = (add_script and add_script[0].lower() == "y")
+    
     try:
         os.mkdir(os.path.join(project, button_id))
     except:
@@ -76,8 +86,12 @@ def main():
     with open(os.path.join(project, button_id, "strings"), 'w+') as strings:
         strings.write("%s.label=%s\n%s.tooltip=%s" % (button_id, label, button_id, tooltip))
     
-    with open(os.path.join(project, button_id, "%s.xul" % app), 'w+') as xul:
-        xul.write("""<toolbarbutton
+    for app in apps:
+        if add_js:
+            with open(os.path.join(project, button_id, "%s.js" % app), 'w+') as js:
+                js.write("""%s: function() {\n}\n""" % function)
+        with open(os.path.join(project, button_id, "%s.xul" % app), 'w+') as xul:
+            xul.write("""<toolbarbutton
 	class="toolbarbutton-1 chromeclass-toolbar-additional"
 	id="%s"
 	label="&%s.label;"
