197c8457c0f4e2974b99f9ac1555ad067a6b046d
codefisher <mail@codefisher.org>
2015-04-17 22:59:41 +0200

    fix to problem in the app_versions.py after update for py3

--- builder/app_versions.py
+++ builder/app_versions.py
@@ -10,7 +10,7 @@ AMO_VERSION_PAGE = "https://addons.mozilla.org/en-US/firefox/pages/appversions/"
 class AppVersionParser(HTMLParser):
 
     def __init__(self):
-        HTMLParser.HTMLParser.__init__(self)
+        super(HTMLParser, self).__init__()
         self._apps = []
         self._current = None
         self._read = False
