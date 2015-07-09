4934418de3a4d846c7f37272df232624beaf098b
codefisher <mail@codefisher.org>
2015-07-09 18:16:42 +1000

    Fixed the pref watcher

--- files/lib/settings.js
+++ files/lib/settings.js
@@ -1,27 +1,26 @@
 settingWatcher: function(pref, func) {
 	this.prefs = toolbar_buttons.interfaces.PrefService.getBranch(pref);
 	this.prefs.QueryInterface(Components.interfaces.nsIPrefBranch2);
-	this.pref = pref;
-	this.func = func;
+
+	var observer = {
+		observe: function(subject, topic, data) {
+			if (topic != "nsPref:changed") {
+				return;
+			}
+			try {
+				func(subject, topic, data);
+			} catch(e) {} // button might not exist
+		}
+	};
 
 	this.startup = function() {
-		this.prefs.addObserver("", this, false);
-		var self = this;
+		this.prefs.addObserver("", observer, false);
 		toolbar_buttons.registerCleanUpFunction(function() {
-			self.prefs.removeObserver("", self);
+			self.prefs.removeObserver("", observer);
 		});
 	};
 
 	this.shutdown = function() {
-		this.prefs.removeObserver("", this);
-	};
-
-	this.observe = function(subject, topic, data) {
-		if (topic != "nsPref:changed") {
-			return;
-		}
-		try {
-			this.func(subject, topic, data);
-		} catch(e) {} // button might not exist
+		this.prefs.removeObserver("", observer);
 	};
 }
