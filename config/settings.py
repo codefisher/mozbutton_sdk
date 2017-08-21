56a5430b221c8c4560a52668c9d47100fc9558c1
codefisher <mail@codefisher.org>
2017-08-21 20:42:56 +1000

    Dropping Firefox out.

--- config/settings.py
+++ config/settings.py
@@ -106,11 +106,10 @@ config = {
     # the below sets application support
     "applications_data": {
         "browser":   (
-                      ("Firefox", "{ec8030f7-c20a-464f-9b0e-13a3a9e97384}", "42.0", "57.0"),
                       ("Pale Moon", "{8de7fcbb-c55c-4fbe-bfc5-fc555c87dbc4}", "20.0", "25.*")
                       ),
         "messenger": (
-                      ("Thunderbird", "{3550f703-e582-4d05-9a08-453d09bdfdc6}","20.0", "42.0"),
+                      ("Thunderbird", "{3550f703-e582-4d05-9a08-453d09bdfdc6}","20.0", "57.0"),
                       ("Postbox", "postbox@postbox-inc.com", "1.0.0", "3.0.*")
                       ),
         #"calendar":  (
