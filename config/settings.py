dc66edbd1963d29848ce0d91461d0ab831699d6a
codefisher <testo.moz@gmail.com>
2018-10-11 08:12:50 +1100

    Bumped max version.

--- config/settings.py
+++ config/settings.py
@@ -107,11 +107,11 @@ config = {
     # the below sets application support
     "applications_data": {
         "browser":   (
-                      #("Firefox", "{ec8030f7-c20a-464f-9b0e-13a3a9e97384}", "42.0", "54.0"),
-                      ("Pale Moon", "{8de7fcbb-c55c-4fbe-bfc5-fc555c87dbc4}", "20.0", "25.*"),
+                      #("Firefox", "{ec8030f7-c20a-464f-9b0e-13a3a9e97384}", "42.0", "58.0"),
+                      ("Pale Moon", "{8de7fcbb-c55c-4fbe-bfc5-fc555c87dbc4}", "20.0", "40.*"),
                       ),
         "messenger": (
-                      ("Thunderbird", "{3550f703-e582-4d05-9a08-453d09bdfdc6}","20.0", "57.0"),
+                      ("Thunderbird", "{3550f703-e582-4d05-9a08-453d09bdfdc6}","20.0", "65.0"),
                       ("Postbox", "postbox@postbox-inc.com", "1.0.0", "3.0.*")
                       ),
         #"calendar":  (
